b342311584162b687ef4a3329bc2821dc21aee63
Kevin Rich <kevirich@umpquabank.com>
2021-04-30 15:38:26 -0700

    added call to apim

--- api/GetPet/index.js
+++ api/GetPet/index.js
@@ -1,8 +1,31 @@
+var fetch = require('fetch');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request for Pet');
+
+    const url = `${process.env.GREYBILL_PETS_BASE_URL}/pet/1`;
+    const headers = {
+        'Cache-Control': 'no-cache',
+        'Ocp-Apim-Subscription-Key': process.env.GREYBILL_PETS_API_KEY
+    };
+
+    async function getPet(){
+        try{
+            let result = await fetch(url, {headers: headers});
+            context.log(result);
+            return await result.json();
+        } catch (error) {
+            context.log(error);
+        }
+    }
+
+    let pet = await getPet();
+
+    context.log(pet);
+
     context.res = {
       body: {
-        text: `w00f!`
+        text: `w00f! ${pet.name}`
       }
     };
   };
