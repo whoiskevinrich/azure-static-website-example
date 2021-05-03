7204d0e6487054da227134718b4ba00bf6bcf1a4
Kevin Rich <kevirich@umpquabank.com>
2021-05-03 07:54:39 -0700

    http request

--- api/GetPet/index.js
+++ api/GetPet/index.js
@@ -1,4 +1,4 @@
-var fetch = require('fetch');
+var fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request for Pet');
@@ -11,7 +11,11 @@ module.exports = async function (context, req) {
 
     async function getPet(){
         try{
+            // turn off cert issues; something off about the network
+            //  https://stackoverflow.com/questions/51995925/node-fetch-request-fails-on-server-unable-to-get-local-issuer-certificate
+            process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
             let result = await fetch(url, {headers: headers});
+            process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 1;
             context.log(result);
             return await result.json();
         } catch (error) {
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 
 - ✅ Host a static javascript web application on Azure
 - ✅ Read from configuration file
-- ⬛ Read from external API
+- ✅ Read from external API
 - ⬛ Require Authentication from Azure Active Directory
 - ⬛ Read from internal API Managed endpoint
 
@@ -27,3 +27,4 @@ Running Site & Backend Locally:
 
 - [Quickstart: Building your first static site in the Azure portal](https://docs.microsoft.com/en-us/azure/static-web-apps/get-started-portal?tabs=vanilla-javascript)
 - [Set up local development for Azure Static Web Apps Preview](https://docs.microsoft.com/en-us/azure/static-web-apps/local-development)
+- [Making API requests in JavaScript Azure Function](https://dzhavat.github.io/2019/07/09/making-http-requests-inside-azure-functions.html)
