ec233fa30895546ec956aef56d4a53f115e4196c
Kevin Rich <whoiskevinrich@gmail.com>
2021-08-07 09:21:21 -0700

    update config value name

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,6 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request for Message');
     context.res = {
-      body: { text: process.env.PHONE_NUMBER }
+      body: { text: process.env.MY_CONFIG_VALUE }
     };
   };
